dfeeed15d23239587b840e16f97cf37fe059a890
pjh347 <gownj333@gmail.com>
2025-11-10 16:38:43 +0900

    💩@State 사용

--- S25W10Mvvm/ContentView.swift
+++ S25W10Mvvm/ContentView.swift
@@ -1,12 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 100
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(count)")
+            
+            HStack {
+                Button("감소") {
+                    count -= 1
+                }
+                Button("증가") {
+                    count += 1
+                }
+            }
         }
         .padding()
     }
