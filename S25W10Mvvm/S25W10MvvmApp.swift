97bc3c1324907df38f0692f2a30641e22fe45e9b
pjh347 <gownj333@gmail.com>
2025-11-10 16:09:58 +0900

    Initial Commit

---
+++ S25W10Mvvm/S25W10MvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  S25W10MvvmApp.swift
+//  S25W10Mvvm
+//
+//  Created by 박준형 on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct S25W10MvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
